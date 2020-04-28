fd559e721bd6a244d951f6b2e0d6e8023408b837
Fabi <Fabi@DESKTOP-U1UF3M0.fritz.box>
2020-04-28 20:42:30 +0200

    ,

--- tst/src/tst/gerere.java
+++ tst/src/tst/gerere.java
@@ -4,11 +4,11 @@ public class gerere {
 
 	public static void main(String[] args) {
 
-		System.out.println("hax");      //penisweewrt
-int i = 13;
-for (int j = 0; j < 10; j++) {
-  System.out.println(i*j);
-}
+		System.out.println("hax"); // penis
+		int i = 13;
+		for (int j = 0; j < 10; j++) {
+			System.out.println(i * j);
+		}
 
 	}
 }
